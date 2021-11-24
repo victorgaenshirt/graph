3fb592ac8bb4ff27ef14cb5a7497de7d5a0cc307
captainjerk01 <stegmaierchristian@gmail.com>
2021-11-24 02:45:29 +0100

    TopologicalSort funktioniert. Output für den Integer-Graphen gut, bei der Kleider-Liste bin ich mir nicht sicher. Im Grunde stimmts aber ist es so auch gewollt?

--- aufg2/src/main/java/directedGraph/TopologicalSort.java
+++ aufg2/src/main/java/directedGraph/TopologicalSort.java
@@ -88,7 +88,10 @@ public class 	TopologicalSort<V> {
 		TopologicalSort<Integer> ts = new TopologicalSort<>(g);
 		
 		if (ts.topologicalSortedList() != null) {
-			System.out.println(ts.topologicalSortedList()); // [1, 2, 3, 4, 5, 6, 7]
+			System.out.println(ts.topologicalSortedList());
+
+		//sollte doch eigentlich ein korrekte Lösung darstellen:
+		// [Mütze, Socken, Unterhemd, Unterhose, Hemd, Hose, Pulli, Gürtel, Schuhe, Mantel, Schal, Handschuhe]
 		}
 
 
